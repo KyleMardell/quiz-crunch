192642fb65d6fd7f71c9994d3a55acc87aef14db
Kyle Mardell <151871777+KyleMardell@users.noreply.github.com>
2024-04-15 22:01:30 +0100

    Add code to switch between sections from the welcome section

--- assets/js/quiz-script.js
+++ assets/js/quiz-script.js
@@ -1,1 +1,51 @@
-console.log("Hello World!");
+// Get sections from DOM
+const welcomeSection = document.getElementById("welcome-section");
+const choicesSection = document.getElementById("choices-section");
+const questionSection = document.getElementById("question-section");
+const scoreSection = document.getElementById("score-section");
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    // Display welcome section, hide all other sections
+    document.getElementById("header-text").addEventListener("click", function() {
+        console.log("logo clicked");
+        displayWelcomeSection();
+    });
+
+    document.getElementById("header-text").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            displayWelcomeSection();
+        }
+    });
+
+    // Display user choice section when clicked
+    document.getElementById("user-quiz-btn").addEventListener("click", function() {
+        console.log("choose quiz clicked");
+        toggleSectionsDisplay(welcomeSection, choicesSection);
+    });
+
+
+    // Display question-secion when clicked
+    document.getElementById("highscore-quiz-btn").addEventListener("click", function() {
+        console.log("highscore quiz clicked");
+        toggleSectionsDisplay(welcomeSection, questionSection);
+    });
+})
+
+/**
+ * Toggle the display style (flex, none)
+ * of the two entered sections
+ */
+function toggleSectionsDisplay(currentSection, newSection) {
+    currentSection.style.display = "none";
+    newSection.style.display = "flex";
+}
+
+function displayWelcomeSection() {
+    choicesSection.style.display = "none";
+    questionSection.style.display = "none";
+    scoreSection.style.display = "none";
+    welcomeSection.style.display = "flex";
+}
+
+
